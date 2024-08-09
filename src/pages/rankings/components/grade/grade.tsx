ec1213a08a572fb0a4ace557972fb17cdf0a5057
尹孟佳 <3495314473@qq.com>
2024-08-09 10:37:10 +0800

    排行榜静态页面v2

--- src/pages/rankings/components/grade/grade.tsx
+++ src/pages/rankings/components/grade/grade.tsx
@@ -1,16 +1,36 @@
-import React, {memo} from 'react'
+import React, {memo, useState} from 'react'
 import type {FC, ReactNode} from 'react'
-import {View} from "@tarojs/components";
+import {Image, Text, View} from "@tarojs/components";
+import WIN from '../../../../public/img/Win.png'
+
 
 interface IProps {
   children?: ReactNode
 }
 
 const Grade: FC<IProps> = () => {
-
+  const [GradeData, setRankingData] = useState([])
   return (
-    <View>
-
+    <View className=' text-[#BEBEBE] text-[30rpx]'>
+      <View className='flex'>
+        <View className='w-[20%]'>
+          <Text>昵称</Text>
+        </View>
+        <View className='flex-1'>
+          <Text>考试名称</Text>
+        </View>
+        <View className='w-[20%]'>
+          <Text>分数(分)</Text>
+        </View>
+      </View>
+      <View className='flex justify-center items-center'>
+        {
+          GradeData.length?null:<View className='flex flex-col items-center'>
+            <Image src={WIN as string} className='w-[300rpx] h-[300rpx] mt-[100rpx]'></Image>
+            <Text className='mt-[30rpx]'>暂无成绩</Text>
+          </View>
+        }
+      </View>
     </View>
   )
 }
--- src/pages/rankings/index.tsx
+++ src/pages/rankings/index.tsx
@@ -1,16 +1,32 @@
-import {memo} from "react";
+import {FC, memo, useEffect, useState} from "react";
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
 import Ranking from "./components/ranking/ranking";
+import Grade from "./components/grade/grade";
 
+interface TabBarProps{
+  getCurrentTab: (current:string)=>void
+}
 
-const TabBar = memo(function (){
-  return <View className='flex border border-solid border-[white] rounded-sm px-[20rpx]'>
-    <View className='pr-[20rpx]  py-[20rpx] border-r border-solid border-[white]'>
+const TabBar:FC<TabBarProps> = memo(function ({getCurrentTab}){
+  const [current, setCurrent] = useState('ranking')
+  function handleRankingClick(){
+    setCurrent('ranking')
+  }
+  function handleGradeClick(){
+    setCurrent('grade')
+
+  }
+
+  useEffect(() => {
+    getCurrentTab(current)
+  }, [current, getCurrentTab]);
+  return <View className='flex border border-solid border-[white] rounded-sm '>
+    <View onClick={handleRankingClick} className={`pr-[20rpx] pl-[20rpx] py-[20rpx] border-r border-solid border-[white] cursor-pointer ${current === 'ranking'?'bg-[white] text-[#63B0F7]':null}`}>
       <Text>排行榜</Text>
     </View>
-    <View className='pl-[20rpx] py-[20rpx]'>
+    <View onClick={handleGradeClick} className={`pl-[20rpx] pr-[20rpx] py-[20rpx] cursor-pointer ${current === 'grade'?'bg-[white] text-[#63B0F7]':null}`}>
       <Text>我的成绩</Text>
     </View>
   </View>
@@ -21,19 +37,35 @@ export default function Rankings () {
   useLoad(() => {
     console.log('Page loaded.')
   })
+  const [currentVal, setCurrentVal] = useState('ranking')
+  function getCurrentTab(current:string){
+    setCurrentVal(current)
+  }
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <View className='rankings h-[100vh] text-[white] '>
       <View className='bg-[#63B0F7] relative flex flex-col h-[40%]'>
         <View className='h-[100%] flex justify-center items-center'>
-          <TabBar></TabBar>
+          <TabBar getCurrentTab={getCurrentTab}></TabBar>
         </View>
         <View className='pb-[35rpx] px-[20rpx] absolute bottom-[60rpx]'>
-          <Text>课堂排名</Text>
+          {
+            currentVal === 'ranking'?<Text>课堂排名</Text>:<Text>我的成绩</Text>
+          }
         </View>
-        <View className='absolute w-[100vw] bottom-[-50rpx] h-[20%] bg-[white] rounded-2xl px-[50rpx] py-[30rpx]'>
+        {
+          currentVal === 'ranking'?
+          <View className='absolute w-[100vw] bottom-[-50rpx] h-[20%] bg-[white] rounded-2xl px-[50rpx] py-[30rpx]'>
             <Ranking></Ranking>
-        </View>
+          </View>:
+          <View className='absolute w-[100vw] bottom-[-50rpx] h-[20%] bg-[white] rounded-2xl px-[50rpx] py-[30rpx]'>
+            <Grade></Grade>
+          </View>
+        }
       </View>
 
 
